5af8d4ce06552038802fc80609ee7558d124c0fd
Sevdjan Bekir <sevdjan.bekir@lights.digital>
2021-12-19 15:25:32 +0200

    created login/register forms and initial home page view

--- app/src/pages/login.jsx
+++ app/src/pages/login.jsx
@@ -1,9 +1,45 @@
 import React from "react";
 
+import { TextField, Button, Card } from "@mui/material";
+import "../App.css";
+
 export default function LoginPage() {
   return (
-    <div>
-      <h1>Login Page!</h1>
+    <div className="formWrapper">
+      <Card
+        sx={{
+          width: 300,
+          height: 350,
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "space-between",
+          padding: 10,
+        }}
+      >
+        <h1>Sign in</h1>
+        <div>
+          <TextField
+            id="standard-basic"
+            label="Email"
+            variant="standard"
+            fullWidth
+          />
+        </div>
+        <div>
+          <TextField
+            id="standard-password-input"
+            label="Password"
+            type="password"
+            variant="standard"
+            fullWidth
+          />
+        </div>
+        <div>
+          <Button variant="contained" fullWidth>
+            Login
+          </Button>
+        </div>
+      </Card>
     </div>
   );
 }
