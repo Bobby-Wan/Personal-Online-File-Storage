c3f118766c0550e0cec7b5e9aefa3e0811a071c9
Bobby-Wan <bobbie.at.97@gmail.com>
2021-12-24 00:35:06 +0200

    added jwt authentication, removed old auth functionality

--- auth.js
+++ auth.js
@@ -1,4 +1,8 @@
-function checkSession(req, res, next) {
+const jwt = require('jsonwebtoken');
+
+const secret = process.env.SECRET;
+
+module.exports.checkSession = function(req, res, next) {
   if (req.cookies.user_sid && req.session.username) {
     res.redirect("/");
   }
@@ -7,14 +11,21 @@ function checkSession(req, res, next) {
   }
 }
 
-function auth(req, res, next){
-  if(!req.cookies.user_sid || !req.session.username){
-    return res.redirect('/login');
-  }
-  next();
+module.exports.createToken = function(payload){
+  return new Promise((resolve, reject)=>{
+    const { password, ...payloadWithoutPassword } = payload;
+    jwt.sign(payloadWithoutPassword, secret, {expiresIn:'1h'}, (err, token)=>{
+      if(err) { return void reject(err); }
+      resolve(token);
+    });
+  });
 }
 
-module.exports = {
-  auth,
-  checkSession
-};
+module.exports.verify = function(token){
+  return new Promise((resolve, reject)=>{
+    jwt.verify(token, secret, (err, decoded)=>{
+      if(err) { return void reject(err); }
+      resolve(decoded);
+    });
+  });
+}
