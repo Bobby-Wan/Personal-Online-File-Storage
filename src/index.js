cf602bb5594de1820a65e81379a7c4b06b6ee36d
Bobby-Wan <bobbie.at.97@gmail.com>
2022-02-24 00:50:45 +0200

    removed old session and hbs engines

--- src/index.js
+++ src/index.js
@@ -23,30 +23,6 @@ app.use(morgan("dev"));
 InitiateMongoServer();
 
 app.use(cookierParser());
-//FIX session secret, save in env variable
-app.use(
-  session({
-    secret: "Shh, its a secret!",
-    store: new MemoryStore({
-      //remove old sessions every 2 hours
-      checkPeriod: 7200000,
-    }),
-    name: "user_sid",
-    cookie: {
-      //1 hour
-      maxAge: 3600000,
-    },
-    resave: false,
-    saveUninitialized: false,
-  })
-);
-
-app.engine(
-  "hbs",
-  exphbs({
-    extname: ".hbs",
-  })
-);
 
 app.set("view engine", "hbs");
 
@@ -55,6 +31,7 @@ process.env.PORT = 8080;
 app.use("/test", bodyParser.text({ type: "text/plain" }));
 app.use(bodyParser.json());
 app.use(router);
+
 
 //HTTPS version
 // const options = {
