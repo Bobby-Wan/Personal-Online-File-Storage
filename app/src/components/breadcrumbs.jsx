42420ad69a8744c3b4fd75cb5c3cc14b211025bc
Sevdjan Bekir <sevdjan.bekir@lights.digital>
2022-02-25 14:22:21 +0200

    fixed breadcrumbs glitch on click

--- app/src/components/breadcrumbs.jsx
+++ app/src/components/breadcrumbs.jsx
@@ -5,7 +5,7 @@ import { Breadcrumbs, Link, Typography } from "@mui/material";
 
 export default function BreadcrumbsNav(props) {
   const [locations, setLocations] = useState(localStorage.getItem("path"));
-  
+
   const splitLocations = () => {
     if (locations !== "/" && typeof locations === "string") {
       const splited = locations.split("/").filter((loc) => loc !== "");
@@ -15,23 +15,34 @@ export default function BreadcrumbsNav(props) {
 
   useEffect(() => {
     splitLocations();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   return (
-    <div
-      role="presentation"
-      onClick={(e) => {
-        e.preventDefault();
-        props.handleClick(e.target.innerText);
-      }}
-    >
+    <div role="presentation">
       <Breadcrumbs aria-label="breadcrumb" style={{ margin: "35px 100px" }}>
-        <Link underline="hover" color="inherit" href="/">
+        <Link
+          underline="hover"
+          color="inherit"
+          href="/"
+          onClick={(e) => {
+            e.preventDefault();
+            props.handleClick("/");
+          }}
+        >
           Home
         </Link>
         {Array.isArray(locations) &&
           locations.map((location) => (
-            <Link underline="hover" color="inherit" href={location}>
+            <Link
+              underline="hover"
+              color="inherit"
+              href={location}
+              onClick={(e) => {
+                e.preventDefault();
+                props.handleClick(e.target.innerText);
+              }}
+            >
               {location}
             </Link>
           ))}
