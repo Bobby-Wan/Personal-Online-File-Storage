0cdef952c35c20541f6d78d4c2b7db778b5d6886
Bobby-Wan <bobbie.at.97@gmail.com>
2022-02-25 19:28:06 +0200

    cors update

--- src/index.js
+++ src/index.js
@@ -15,7 +15,10 @@ var cors = require("cors");
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: "http://localhost:3000",
+  credentials: true
+}));
 
 app.use(morgan("dev"));
 
